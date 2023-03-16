daf075fe1d431a265e8a938784cc1131fb0e1bb8
maud.marty <maud.marty@inrae.fr>
2023-03-16 12:19:29 +0100

    Revert "Page layout"
    This reverts commit dba1ac1637fbee121e9fb312fa1d009e3f2edc01.

--- etl/transform/elasticsearch.py
+++ etl/transform/elasticsearch.py
@@ -20,52 +20,6 @@ from etl.transform.uri import UriIndex
 NB_THREADS = max(int(multiprocessing.cpu_count() * 0.75), 2)
 CHUNK_SIZE = 500
 
-#############
-# REF
-# 1. use of dict rather than in memory db
-#   - https://www.oreilly.com/library/view/high-performance-python/9781449361747/ch04.html
-#   - https://fr.wikipedia.org/wiki/Analyse_de_la_complexit%C3%A9_des_algorithmes
-#############
-
-document_types = [
-    {
-        "document-type": "germplasm",
-        "source-entity": "germplasm",
-        "mandatory" : True
-    },
-    {
-        "document-type": "germplasmAttribute",
-        "source-entity": "germplasmAttribute"
-    },
-    {
-        "document-type": "germplasmPedigree",
-        "source-entity": "germplasmPedigree"
-    },
-    {
-        "document-type": "germplasmProgeny",
-        "source-entity": "germplasmProgeny"
-    },
-    {
-        "document-type": "location",
-        "source-entity": "location"
-    },
-    {
-        "document-type": "observationUnit",
-        "source-entity": "observationUnit"
-    },
-    {
-        "document-type": "program",
-        "source-entity": "program"
-    },
-    {
-        "document-type": "study",
-        "source-entity": "study"
-    },
-    {
-        "document-type": "trial",
-        "source-entity": "trial"
-    }
-]
 
 def remove_html_tags(text):
     """
@@ -122,6 +76,119 @@ def rm_tags(source_json_dir):
                 new_json_file.write('\n')
 
 
+def is_checkpoint(n):
+    return n > 0 and n % 10000 == 0
+
+
+def generate_elasticsearch_document(options):
+    document_type, document_transform, uri_data_index, data_json = options
+    document = json.loads(data_json)
+    if document_transform:
+        resolved = remove_empty(resolve(document_transform, document, uri_data_index))
+        document.update(resolved)
+    return document_type, document
+
+
+def generate_elasticsearch_documents(restricted_documents, document_configs_by_entity,
+                                      uri_data_index: UriIndex, pool: Pool, logger: Logger):
+    """
+    Produces and iterable of tuples (of document type and document) generated using the
+    document templates in configuration. (not threaded)
+    """
+    logger.debug("Preparing documents generation...")
+    document_count = 0
+    for entity_name in uri_data_index.keys():
+        uri_index = uri_data_index[entity_name]
+        for data_json in uri_index.values():
+            document_configs = document_configs_by_entity.get(entity_name) or [{}]
+            for document_config in document_configs:
+                document_type = document_config.get('document-type') or entity_name
+                if restricted_documents and document_type not in restricted_documents:
+                    continue
+                document_transform = document_config.get('document-transform')
+                document_count += 1
+                yield generate_elasticsearch_document((document_type, document_transform, uri_data_index, data_json))
+        uri_index.close()
+    logger.debug(f"Generated {document_count} documents.")
+
+
+def validate_documents(document_tuples, validation_schemas, logger):
+    """
+    Consumes an iterable of document type and document tuples and validate each document according to its
+    validation schema defined in configuration.
+    Produces the same type of iterable as input
+    """
+    logger.debug("Validating documents JSON schemas...")
+    document_count = 0
+    for document_type, document in document_tuples:
+        document_count += 1
+        schema = validation_schemas.get(document_type)
+        try:
+            schema and jsonschema.validate(document, schema)
+        except (SchemaError, ValidationError) as e:
+            raise Exception(
+                f"Could not validate document {document} \n"
+                f"of type {document_type} using the provided json schema:\n {schema}"
+            ) from e
+        yield document_type, document
+    logger.debug(f"Validated {document_count} documents.")
+
+
+def dump_clean_in_json_files(source_dir, source_name, logger, documents_tuples):
+    """
+    Consumes an iterable of document tuples and clean email
+    """
+    logger.debug("Saving documents to json files...")
+
+    json_dict = dict()
+    document_count = 0
+    for document_header, document in documents_tuples:
+
+        # Hide email
+        if ("email" in document):
+                document["email"] = document["email"].replace('@', '_')
+
+        if ("contacts" in document):
+            for contact in document["contacts"]:
+                if "email" in contact:
+                    contact["email"] = contact["email"].replace('@', '_')
+
+        if document_header not in json_dict:
+            json_dict[document_header] = []
+
+        json_dict[document_header].append(document)
+
+        if ("node" not in document):
+            document["node"] = source_name
+            document["databaseName"] = "brapi@" + source_name
+
+        if ("source" not in document):
+            document["source"] = source_name
+
+        document_count += 1
+        if is_checkpoint(document_count):
+            logger.debug(f"checkpoint: {document_count} documents saved")
+
+    save_json(source_dir, json_dict)
+
+    logger.debug(f"Total of {document_count} documents saved in json files.")
+
+
+def save_json(source_dir, json_dict):
+    for type, document in json_dict.items():
+        file_number = 1
+        saved_documents = 0
+        while saved_documents < len(document):
+            with open(source_dir + "/" + type + '-' + str(file_number) + '.json', 'w') as f:
+                json.dump(document[saved_documents:file_number*10000], f, ensure_ascii=False)
+            with open(source_dir + "/" + type + '-' + str(file_number) + '.json', 'rb') as f:
+                with gzip.open(source_dir + "/" + type + '-' + str(file_number) + '.json.gz', 'wb') as f_out:
+                    shutil.copyfileobj(f, f_out)
+            os.remove(source_dir + "/" + type + '-' + str(file_number) + '.json')
+            file_number += 1
+            saved_documents += 10000
+
+
 def get_document_configs_by_entity(document_configs):
     by_entity = dict()
     for document_config in document_configs:
@@ -131,55 +198,44 @@ def get_document_configs_by_entity(document_configs):
         by_entity[entity].append(document_config)
     return by_entity
 
-def get_generate_uri(source: dict, entity: str, data: dict) -> str:
+
+def get_required_entities(document_configs, source_json_dir):
     """
-    Get/Generate URI from BrAPI object or generate one
+    Returns set of required entities for all documents in configuration
     """
-    pui_field = entity + 'PUI'
-    data_uri = data.get(pui_field)
+    source_entities = set(remove_none(map(lambda d: d.get('source-entity'), document_configs)))
 
-    if data_uri and rfc3987.match(data_uri, rule='URI'):
-        # The original PUI is a valid URI
-        return data_uri
+    def collect_entities(parsed_template):
+        if is_list_like(parsed_template):
+            return set(flatten_it(map(collect_entities, parsed_template)))
+        if isinstance(parsed_template, dict):
+            if '{lark}' in parsed_template:
+                entities = set()
+                for object_path in as_list(get_in(parsed_template, ['start', 'object_path'])):
+                    fields = get_in(object_path, ['field_path', 'FIELD'])
+                    match = re.search("^(\\w+)DbId(s?)$", fields[-1])
+                    if match:
+                        entities.add(match.groups()[0])
+                return entities
+            return set(flatten_it(map(collect_entities, parsed_template.values())))
+        return set()
 
-    source_id = urllib.parse.quote(source['schema:identifier'])
-    data_id = get_identifier(entity, data)
-    if not data_uri:
-        # Generate URI from source id, entity name and data id
-        encoded_entity = urllib.parse.quote(entity)
-        encoded_id = urllib.parse.quote(data_id)
-        data_uri = f"urn:{source_id}/{encoded_entity}/{encoded_id}"
-    else:
-        # Generate URI by prepending the original URI with the source identifier
-        encoded_uri = urllib.parse.quote(data_uri)
-        data_uri = f"urn:{source_id}/{encoded_uri}"
+    document_transforms = remove_none(map(lambda d: d.get('document-transform'), document_configs))
+    required_entities = source_entities.union(flatten_it(map(collect_entities, document_transforms)))
 
-    if not rfc3987.match(data_uri, rule='URI'):
-        raise Exception(f'Could not get or create a correct URI for "{entity}" object id "{data_id}"'
-                        f' (malformed URI: "{data_uri}")')
-    return data_uri
-
-
-def load_input_json(source, doc_types, source_json_dir, config):
-    data_dict = {}
     if source_json_dir:
-        #all_files = list_entity_files(source_json_dir)
-        #filtered_files = list(filter(lambda x: x[0] in source_entities, all_files))
-        for document_type in doc_types:
-            input_json_filepath = source_json_dir + "/" + document_type["document-type"] + ".json"
-            data_dict[document_type["document-type"]] = {}
-            try:
-                with open(input_json_filepath, 'r') as json_file:
-                    json_list = list(json_file)
-                    for json_line in json_list:
-                        data = json.loads(json_line)
-                        uri = get_generated_uri(source, document_type["document-type"], data)
-                        data_dict[document_type["document-type"]][uri] = data
-            #                    links = get_entity_links(data, 'DbId')
-            #                    entity_names = set(map(first, links))
-            except FileNotFoundError as e:
-                print("No "+document_type["document-type"]+" in "+source['schema:identifier'])
-    return data_dict
+        all_files = list_entity_files(source_json_dir)
+        filtered_files = list(filter(lambda x: x[0] in source_entities, all_files))
+        for entity_name, file_path in filtered_files:
+            with open(file_path, 'r') as file:
+                line = file.readline()
+                if line:
+                    data = json.loads(line)
+                    links = get_entity_links(data, 'DbId')
+                    entity_names = set(map(first, links))
+                    required_entities.update(entity_names)
+
+    return required_entities
 
 
 def transform_source(source, transform_config, source_json_dir, source_bulk_dir, config):
@@ -189,11 +245,27 @@ def transform_source(source, transform_config, source_json_dir, source_bulk_dir,
     failed_dir = source_bulk_dir + '-failed'
     if os.path.exists(failed_dir):
         shutil.rmtree(failed_dir, ignore_errors=True)
+    validation_schemas = transform_config['validation-schemas']
     source_name = source['schema:identifier']
 
     action = 'transform-es-' + source_name
     log_file = get_file_path([config['log-dir'], action], ext='.log', recreate=True)
     logger = create_logger(action, log_file, config['options']['verbose'])
+    pool = Pool(NB_THREADS)
+
+    document_configs = transform_config['documents']
+    document_configs_by_entity = get_document_configs_by_entity(document_configs)
+    restricted_documents = transform_config.get('restricted-documents')
+    if restricted_documents:
+        document_types = set([doc['document-type'] for doc in document_configs])
+        # unknown_doc_types = restricted_documents.difference(document_types)
+        # if unknown_doc_types:
+        #     raise Exception('Invalid document type(s) given: \'{}\''.format(', '.join(restricted_documents)))
+        restricted_documents = restricted_documents.intersection(document_types)
+
+        transform_config['documents'] = [
+            document for document in document_configs if document['document-type'] in restricted_documents
+        ]
 
     logger.info("Transforming BrAPI to Elasticsearch documents...")
     try:
@@ -203,28 +275,35 @@ def transform_source(source, transform_config, source_json_dir, source_bulk_dir,
                 'Please make sure you have run the BrAPI extraction before trying to launch the transformation process.'
             )
 
-        # TODO: this should be generalised : detect sources that are not jsonl and turn it into the right format
         if source_name == 'EVA':
             logger.info("Flattening EVA data...")
             json_to_jsonl(source_json_dir)
             rm_tags(source_json_dir)
 
-        # Load each file (aka document type) in a per source hash.
-        # structure or the keys: documenttype>documentDbId
-        # TODO: call get_generated_uri at load time
-        get_generated_uri(source, entity: str, data: dict)
-        # TODO: don't load observationUnit, too big and of little interest.
-        #  Instead stream and do on the fly transform of the relevant dbId at the end of the process
-        current_source_data_dict = load_input_json(source, transform_config, source_json_dir, config)
+        logger.info('Loading data, generating URIs and global identifiers...')
+        uri_data_index = uri.transform_source(source, config)
+
+        logger.info('Generating documents...')
+        documents = generate_elasticsearch_documents(
+            restricted_documents, document_configs_by_entity, uri_data_index, pool, logger
+        )
+
+        # Validate the document schemas
+        validated_documents = validate_documents(documents, validation_schemas, logger)
+
+        # Write the documents in jsonfiles
+        dump_clean_in_json_files(source_bulk_dir, source_name, logger, validated_documents)
+        # shutil.rmtree(tmp_index_dir, ignore_errors=True)
 
         logger.info(f"SUCCEEDED Transforming BrAPI {source_name}.")
-
     except Exception as e:
         logger.debug(traceback.format_exc())
         shutil.move(source_bulk_dir, failed_dir)
         logger.info("FAILED Transforming BrAPI {}.\n"
                     "=> Check the logs ({}) and data ({}) for more details."
                     .format(source_name, log_file, failed_dir))
+
+    pool.close()
 
 
 def main(config):
